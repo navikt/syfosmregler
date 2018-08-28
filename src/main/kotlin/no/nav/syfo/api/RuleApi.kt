220918d87948a620916d65b9c8ff316d1271707f
Kevin Sillerud <kevin.sillerud@gmail.com>
2018-08-28 12:16:16 +0200

    Add some debug logging

--- src/main/kotlin/no/nav/syfo/api/RuleApi.kt
+++ src/main/kotlin/no/nav/syfo/api/RuleApi.kt
@@ -5,17 +5,26 @@ import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import io.ktor.application.ApplicationCall
 import io.ktor.application.call
 import io.ktor.request.receiveStream
+import io.ktor.request.receiveText
 import io.ktor.response.respondWrite
 import io.ktor.routing.Routing
 import io.ktor.routing.post
 import no.nav.syfo.model.Status
 import no.nav.syfo.model.ValidationResult
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 
 val objectMapper: ObjectMapper = ObjectMapper().registerKotlinModule()
+
+val log: Logger = LoggerFactory.getLogger("no.nav.syfo.smregler")
 
 fun Routing.registerRuleApi() {
     post("/v1/rules/validate") {
         call.respondJson {
+            log.info("Got an request to validate rules")
+            if (log.isDebugEnabled) {
+                log.debug(call.receiveText())
+            }
             ValidationResult(
                     status = Status.OK,
                     ruleHits = listOf()
@@ -24,7 +33,7 @@ fun Routing.registerRuleApi() {
     }
 }
 
-suspend fun ApplicationCall.respondJson(jsonCallback: () -> Any) {
+suspend fun ApplicationCall.respondJson(jsonCallback: suspend () -> Any) {
     respondWrite {
         objectMapper.writeValue(this, jsonCallback())
     }
