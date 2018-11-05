b2a127acf1ecea9e45f308ef70b3d31856d97e86
Kevin Sillerud <kevin.sillerud@gmail.com>
2018-11-05 14:31:00 +0100

    Implement more field based rules

---
+++ src/main/kotlin/no/nav/syfo/Kodeverk.kt
@@ -0,0 +1,7 @@
+package no.nav.syfo
+
+interface Kodeverk {
+    val codeValue: String
+    val text: String
+    val oid: String
+}
--- src/main/kotlin/no/nav/syfo/rules/PreTPSRuleChain.kt
+++ src/main/kotlin/no/nav/syfo/rules/PreTPSRuleChain.kt
@@ -1,6 +1,7 @@
 package no.nav.syfo.rules
 
 import no.kith.xmlstds.msghead._2006_05_24.XMLMsgHead
+import no.nav.model.sm2013.CV
 import no.nav.model.sm2013.HelseOpplysningerArbeidsuforhet
 import no.nav.syfo.Description
 import no.nav.syfo.Diagnosekode
@@ -11,6 +12,8 @@ import no.nav.syfo.get
 import no.nav.syfo.model.Status
 import no.nav.syfo.validation.extractBornDate
 import no.nav.syfo.validation.validatePersonAndDNumber
+import no.nav.syfo.ICD10
+import no.nav.syfo.ICPC2
 import no.trygdeetaten.xml.eiff._1.XMLEIFellesformat
 import no.trygdeetaten.xml.eiff._1.XMLMottakenhetBlokk
 import java.time.DayOfWeek
@@ -62,6 +65,63 @@ enum class ValidationRules(override val ruleId: Int?, override val status: Statu
     @Description("Ukjent diagnosekode type")
     UNKNOWN_DIAGNOSECODE_TYPE(1137, Status.INVALID, { (healthInformation, _) ->
         healthInformation.medisinskVurdering.hovedDiagnose.diagnosekode.s !in Diagnosekode.values()
+    }),
+
+    @Description("Hvis hoveddiagnose er Z-diagnose (ICPC-2), avvises meldingen.")
+    ICPC_2_Z_DIAGNOSE(null, Status.INVALID, { (healthInformation, _) ->
+        // To support not having a main diagnosis and avoid checking for invalid code here we allow null and compare
+        // with the equality operator
+        (healthInformation.medisinskVurdering.hovedDiagnose.diagnosekode.toICPC2()?.first()?.codeValue?.startsWith("Z") == true)
+    }),
+
+    @Description("Hvis hoveddiagnose mangler og det ikke er angitt annen lovfestet fraværsgrunn, avvises meldingen")
+    MAIN_DIAGNOSE_MISSING_AND_MISSING_REASON(null, Status.INVALID, { (healthInformation, _) ->
+        healthInformation.medisinskVurdering.annenFraversArsak == null &&
+                healthInformation.medisinskVurdering.hovedDiagnose.let { it == null || it.diagnosekode == null || it.diagnosekode.v == null }
+    }),
+
+    @Description("Hvis kodeverk ikke er angitt eller korrekt for hoveddiagnose, avvises meldingen.")
+    INVALID_KODEVERK_FOR_MAIN_DIAGNOSE(1540, Status.INVALID, { (healthInformation, _) ->
+        healthInformation.medisinskVurdering.hovedDiagnose.diagnosekode.let { cv ->
+            if (cv.isICPC2()) {
+                ICPC2.values().any { it.codeValue == cv.v }
+            } else {
+                ICD10.values().any { it.codeValue == cv.v }
+            }
+        }
+    }),
+
+    @Description("Hvis sykmeldingsgrad er mindre enn 20% for gradert sykmelding, avvises meldingen")
+    PARTIAL_SICK_LEAVE_PERCENTAGE_TO_LOW(null, Status.INVALID, { (healthInformation, _) ->
+        healthInformation.aktivitet.periode.any {
+            it.gradertSykmelding != null && it.gradertSykmelding.sykmeldingsgrad < 20
+        }
+    }),
+
+    // EI@ tekst "Enkeltstående behandlingsdager er angitt."
+    @Description("Hvis behandlingsdager er angitt sendes meldingen til manuell behandling.")
+    NUMBER_OF_TREATMENT_DAYS_SET(1260, Status.MANUAL_PROCESSING, { (healthInformation, _) ->
+        healthInformation.aktivitet.periode.any { it.behandlingsdager != null }
+    }),
+
+    // TODO: '1608','Kun reisetilskudd er angitt. Melding sendt til oppfølging i Arena, skal ikke registreres i Infotrygd.',3,'1' ???
+    @Description("Hvis sykmeldingen angir reisetilskudd går meldingen til manuell behandling.")
+    TRAVEL_SUBSIDY_SPECIFIED(1608, Status.MANUAL_PROCESSING, { (healthInformation, _) ->
+        healthInformation.aktivitet.periode.any { it.isReisetilskudd == true } // Can be null, so use equality
+    }),
+
+    @Description("Hvis medisinske eller arbeidsplassrelaterte årsaker ved 100% sykmelding ikke er oppgitt og sykmeldingen ikke er \"forenklet\"")
+    NO_MEDICAL_OR_WORKPLACE_RELATED_REASONS(null, Status.INVALID, { (healthInformation, _) ->
+        healthInformation.medisinskVurdering.hovedDiagnose.diagnosekode.toICPC2()?.any { icpc2 -> icpc2 in diagnoseCodesSimplified } == true && !healthInformation.aktivitet.periode.all {
+            (it.gradertSykmelding == null || it.gradertSykmelding.sykmeldingsgrad == 100) &&
+                    it.aktivitetIkkeMulig?.arbeidsplassen?.arsakskode != null &&
+                    it.aktivitetIkkeMulig.medisinskeArsaker?.arsakskode != null
+        } == true
+    }),
+
+    @Description("Hvis utdypende opplysninger om medisinske eller arbeidsplassrelaterte årsaker ved 100% sykmelding ikke er oppgitt  ved  8, 17, 39 uker før regelsettversjon \"2\" er innført skal sykmeldingen avvises")
+    WHAT_EVEN_IS_THIS_RULE(null, Status.INVALID, { (healthInformation, _) ->
+        true // TODO
     })
 }
 
@@ -158,9 +218,24 @@ enum class PeriodLogicRuleChain(override val ruleId: Int?, override val status: 
     }),
 
     @Description("Hvis gradert sykmelding og reisetilskudd er oppgitt for samme periode sendes meldingen til manuell behandling.")
-    GRADUAL_SYKMELDING_COMBINED_WITH_TRAVEL(null, Status.MANUAL_PROCESSING, { (healthInformation, _) ->
+    GRADUAL_SYKMELDING_COMBINED_WITH_TRAVEL(1250, Status.MANUAL_PROCESSING, { (healthInformation, _) ->
         healthInformation.aktivitet.periode.any { it.gradertSykmelding != null && it.isReisetilskudd }
-    })
+    }),
+
+    @Description("Fom-dato i ny sykmelding som er en forlengelse kan maks være tilbakedatert 1 mnd fra signaturdato. Skal telles.")
+    BACKDATING_SYKMELDING_EXTENSION(null, Status.INVALID, { (healthInformation, ruleMetadata) ->
+        healthInformation.aktivitet.periode.sortedFOMDate().first().minusMonths(1) < ruleMetadata.signatureDate
+    }),
+
+    @Description("Hvis sykmeldingen er fremdatert mer enn 30 dager etter konsultasjonsdato/signaturdato avvises meldingen.")
+    PRE_DATED(null, Status.INVALID, { (healthInformation, ruleMetadata) ->
+        healthInformation.aktivitet.periode.sortedFOMDate().first() > ruleMetadata.signatureDate.plusDays(30)
+    }),
+
+    @Description("Hvis sykmeldingens sluttdato er mer enn ett år frem i tid, avvises meldingen.")
+    END_DATE(null, Status.INVALID, { (healthInformation, ruleMetadata) ->
+        healthInformation.aktivitet.periode.sortedTOMDate().last() > ruleMetadata.receivedDate.plusYears(1)
+    }),
 }
 
 fun List<HelseOpplysningerArbeidsuforhet.Aktivitet.Periode>.sortedFOMDate(): List<ZonedDateTime> =
@@ -182,3 +257,11 @@ fun HelseOpplysningerArbeidsuforhet.Aktivitet.Periode.range(): ClosedRange<Zoned
 
 operator fun ClosedRange<ZonedDateTime>.contains(v: XMLGregorianCalendar) =
         v.toZoned() > start || v.toZoned() < endInclusive
+
+fun CV.isICPC2(): Boolean = v == ICPC2.A01.oid
+
+fun CV.toICPC2(): List<ICPC2>? = if (isICPC2()) { listOfNotNull(ICPC2.values().find { it.codeValue == v }) } else { ICD10.values().find { it.codeValue == v }?.icpc2 }
+
+val diagnoseCodesSimplified = listOf(
+        ICPC2.D70, ICPC2.D73, ICPC2.F70, ICPC2.F73, ICPC2.H71, ICPC2.R72, ICPC2.R74, ICPC2.R75, ICPC2.R76, ICPC2.R77, ICPC2.R78, ICPC2.R80, ICPC2.R81, ICPC2.U71
+)
