9f2b6032fb13518534f6950d49121900a4660f05
Joakim kartveit <joakim.kartveit@nav.no>
2019-02-13 15:07:37 +0100

    Added new metrics for rule status

--- src/main/kotlin/no/nav/syfo/MetricsRegistry.kt
+++ src/main/kotlin/no/nav/syfo/MetricsRegistry.kt
@@ -9,6 +9,8 @@ val RULE_HIT_COUNTER: Counter = Counter.Builder().namespace(NAMESPACE).name("rul
         .labelNames("rule_name").help("Registers a counter for each rule in the rule set").register()
 val RULE_HIT_SUMMARY: Summary = Summary.Builder().namespace(NAMESPACE).name("rule_hit_summary")
         .labelNames("rule_name").help("Registers a summary for each rule in the rule set").register()
+val RULE_HIT_STATUS_COUNTER: Counter = Counter.Builder().namespace(NAMESPACE).name("rule_hit_status_counter")
+        .labelNames("rule_status").help("Registers a counter for each rule status").register()
 val REST_CALL_SUMMARY: Summary = Summary.Builder().namespace(NAMESPACE).name("rest_call_summary")
         .labelNames("http_endpoint").help("Summary for rest call times").register()
 val SOAP_CALL_SUMMARY: Summary = Summary.Builder().namespace(NAMESPACE).name("soap_call_summary")
