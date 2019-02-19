f82883a9acd230b4a0974854d6115fb223f4f2eb
Joakim kartveit <joakim.kartveit@nav.no>
2019-02-19 08:48:50 +0100

    Updated kotlin jvm to 1.3.11 and removed ktor-client-logging

--- src/main/kotlin/no/nav/syfo/api/LegeSuspensjonClient.kt
+++ src/main/kotlin/no/nav/syfo/api/LegeSuspensjonClient.kt
@@ -7,10 +7,6 @@ import io.ktor.client.HttpClient
 import io.ktor.client.engine.cio.CIO
 import io.ktor.client.features.json.JacksonSerializer
 import io.ktor.client.features.json.JsonFeature
-import io.ktor.client.features.logging.DEFAULT
-import io.ktor.client.features.logging.LogLevel
-import io.ktor.client.features.logging.Logger
-import io.ktor.client.features.logging.Logging
 import io.ktor.client.request.accept
 import io.ktor.client.request.get
 import io.ktor.client.request.headers
@@ -30,10 +26,6 @@ class LegeSuspensjonClient(private val endpointUrl: String, private val credenti
                 registerModule(JavaTimeModule())
                 configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
             }
-        }
-        install(Logging) {
-            logger = Logger.DEFAULT
-            level = LogLevel.INFO
         }
     }
 
