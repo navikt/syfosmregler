1ed2c60e0a2ffb8ce2c3fd6761db0d82c7e3d094
Joakim kartveit <joakim.kartveit@nav.no>
2019-02-26 09:01:18 +0100

    Changed label name for status

--- src/main/kotlin/no/nav/syfo/MetricsRegistry.kt
+++ src/main/kotlin/no/nav/syfo/MetricsRegistry.kt
@@ -6,6 +6,6 @@ import io.prometheus.client.Summary
 const val NAMESPACE = "syfosmregler"
 
 val RULE_HIT_COUNTER: Counter = Counter.Builder().namespace(NAMESPACE).name("rule_hit_counter")
-        .labelNames("rule_name", "status").help("Registers a counter for each rule in the rule set").register()
+        .labelNames("rule_name", "rule_status").help("Registers a counter for each rule in the rule set").register()
 val NETWORK_CALL_SUMMARY: Summary = Summary.Builder().namespace(NAMESPACE).name("network_call_summary")
         .labelNames("http_endpoint").help("Summary for networked call times").register()
