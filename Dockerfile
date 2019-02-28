916eb084fe0a8864b5b2e58a82c727031b622051
Joakim kartveit <joakim.kartveit@nav.no>
2019-02-28 19:53:09 +0100

    Bumping gradle verion to 5.2.1 Converting from gradle groovy to gradle kotlin

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM navikt/java:11
 COPY config-preprod.json .
 COPY config-prod.json .
-COPY build/libs/syfosmregler-*-all.jar app.jar
-ENV JAVA_OPTS='-Dlogback.configurationFile=logback-remote.xml'
+COPY build/libs/*.jar app.jar
+ENV JAVA_OPTS="-Dlogback.configurationFile=logback-remote.xml"
---
+++ build.gradle.kts
@@ -0,0 +1,178 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+import com.github.jengelman.gradle.plugins.shadow.transformers.ServiceFileTransformer
+import no.nils.wsdl2java.Wsdl2JavaTask
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import no.nav.codegen.CodegenTask
+
+group = "no.nav.syfo"
+version = "1.0.10"
+
+val coroutinesVersion = "1.0.0"
+val javaxActivationVersion = "1.1.1"
+val jacksonVersion = "2.9.7"
+val jaxbApiVersion = "2.4.0-b180830.0359"
+val jaxbVersion = "2.3.0.1"
+val kluentVersion = "1.39"
+val ktorVersion = "1.1.3"
+val logbackVersion = "1.2.3"
+val logstashEncoderVersion = "5.1"
+val prometheusVersion = "0.5.0"
+val spekVersion = "2.0.0-rc.1"
+val sykmeldingVersion = "1.1-SNAPSHOT"
+val navPersonv3Version = "3.2.0"
+val cxfVersion = "3.2.7"
+val jaxbBasicAntVersion = "1.11.1"
+val javaxAnnotationApiVersion = "1.3.2"
+val jaxwsToolsVersion = "2.3.1"
+val jaxbRuntimeVersion = "2.4.0-b180830.0438"
+val javaxJaxwsApiVersion = "2.2.1"
+val jfairyVersion = "0.6.2"
+val saajVersion = "1.4.0"
+val commonsTextVersion = "1.4"
+
+tasks.withType<Jar> {
+    manifest.attributes["Main-Class"] = "no.nav.syfo.BootstrapKt"
+}
+
+
+plugins {
+    java
+    id("no.nils.wsdl2java") version "0.10"
+    kotlin("jvm") version "1.3.21"
+    id("org.jmailen.kotlinter") version "1.21.0"
+    id("com.diffplug.gradle.spotless") version "3.14.0"
+    id("com.github.johnrengelman.shadow") version "4.0.4"
+}
+
+
+buildscript {
+    dependencies {
+        classpath("javax.xml.bind:jaxb-api:2.4.0-b180830.0359")
+        classpath("org.glassfish.jaxb:jaxb-runtime:2.4.0-b180830.0438")
+        classpath("com.sun.activation:javax.activation:1.2.0")
+        classpath("com.sun.xml.ws:jaxws-tools:2.3.1") {
+            exclude(group = "com.sun.xml.ws", module = "policy")
+        }
+    }
+}
+
+repositories {
+    maven (url= "https://repo.adeo.no/repository/maven-snapshots/")
+    maven (url= "https://repo.adeo.no/repository/maven-releases/")
+    maven (url= "https://dl.bintray.com/kotlin/ktor")
+    maven (url= "https://dl.bintray.com/spekframework/spek-dev")
+    maven (url= "https://kotlin.bintray.com/kotlinx")
+    mavenCentral()
+    jcenter()
+}
+
+
+dependencies {
+    wsdl2java("javax.annotation:javax.annotation-api:$javaxAnnotationApiVersion")
+    wsdl2java("javax.activation:activation:$javaxActivationVersion")
+    wsdl2java("org.glassfish.jaxb:jaxb-runtime:$jaxbRuntimeVersion")
+    wsdl2java("javax.xml.bind:jaxb-api:$jaxbApiVersion")
+    wsdl2java("javax.xml.ws:jaxws-api:$javaxJaxwsApiVersion")
+    wsdl2java("com.sun.xml.ws:jaxws-tools:$jaxwsToolsVersion") {
+        exclude(group = "com.sun.xml.ws", module = "policy")
+    }
+
+
+    implementation(kotlin("stdlib"))
+
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion")
+    implementation("io.prometheus:simpleclient_hotspot:$prometheusVersion")
+    implementation("io.prometheus:simpleclient_common:$prometheusVersion")
+
+    implementation("io.ktor:ktor-server-netty:$ktorVersion")
+    implementation("io.ktor:ktor-client-apache:$ktorVersion")
+    implementation("io.ktor:ktor-client-auth-basic:$ktorVersion")
+    implementation("io.ktor:ktor-client-jackson:$ktorVersion")
+
+    implementation("ch.qos.logback:logback-classic:$logbackVersion")
+    implementation("net.logstash.logback:logstash-logback-encoder:$logstashEncoderVersion")
+
+    implementation("com.fasterxml.jackson.module:jackson-module-jaxb-annotations:$jacksonVersion")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:$jacksonVersion")
+    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-xml:$jacksonVersion")
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:$jacksonVersion")
+
+    implementation("no.nav.helse.xml:sm2013:$sykmeldingVersion")
+    implementation("no.nav.tjenester:nav-person-v3-tjenestespesifikasjon:$navPersonv3Version")
+
+    implementation("org.apache.commons:commons-text:$commonsTextVersion")
+    implementation("org.apache.cxf:cxf-rt-frontend-jaxws:$cxfVersion")
+    implementation("org.apache.cxf:cxf-rt-features-logging:$cxfVersion")
+    implementation("org.apache.cxf:cxf-rt-transports-http:$cxfVersion")
+    implementation("org.apache.cxf:cxf-rt-ws-security:$cxfVersion")
+
+    implementation("javax.xml.ws:jaxws-api:$javaxJaxwsApiVersion")
+    implementation("javax.annotation:javax.annotation-api:$javaxAnnotationApiVersion")
+    implementation("javax.xml.bind:jaxb-api:$jaxbApiVersion")
+    implementation("org.glassfish.jaxb:jaxb-runtime:$jaxbRuntimeVersion")
+    implementation("javax.activation:activation:$javaxActivationVersion")
+    implementation("com.sun.xml.ws:jaxws-tools:$jaxwsToolsVersion") {
+        exclude(group = "com.sun.xml.ws", module = "policy")
+    }
+
+    implementation("com.sun.xml.messaging.saaj:saaj-impl:$saajVersion")
+
+    testImplementation("org.amshove.kluent:kluent:$kluentVersion")
+    testImplementation("org.spekframework.spek2:spek-dsl-jvm:$spekVersion")
+    testImplementation("com.devskiller:jfairy:$jfairyVersion")
+    testImplementation("io.ktor:ktor-server-test-host:$ktorVersion") {
+        exclude(group = "org.eclipse.jetty") // conflicts with WireMock
+    }
+
+    testImplementation("org.spekframework.spek2:spek-dsl-jvm:$spekVersion") {
+        exclude(group = "org.jetbrains.kotlin")
+    }
+    testRuntimeOnly ("org.spekframework.spek2:spek-runner-junit5:$spekVersion") {
+        exclude(group = "org.jetbrains.kotlin")
+    }
+
+}
+
+sourceSets {
+    main {
+        java {
+            srcDir("$buildDir/generated-sources")
+        }
+    }
+}
+
+val generateDiagnoseCodes = tasks.withType<CodegenTask> {
+    outputDirectory = file("${project.buildDir}/generated-sources")
+}
+
+
+tasks {
+    create("printVersion") {
+        println(project.version)
+    }
+
+    withType<KotlinCompile> {
+        dependsOn("generateDiagnoseCodes","wsdl2java")
+    }
+
+    withType<Wsdl2JavaTask> {
+        wsdlDir = file("$projectDir/src/main/resources/wsdl")
+        wsdlsToGenerate = listOf(
+                mutableListOf("-xjc", "-b", "$projectDir/src/main/resources/xjb/binding.xml", "$projectDir/src/main/resources/wsdl/helsepersonellregisteret.wsdl")
+        )
+    }
+
+    withType<ShadowJar> {
+        transform(ServiceFileTransformer::class.java) {
+            setPath("META-INF/cxf")
+            include("bus-extensions.txt")
+        }
+    }
+
+    withType<Test> {
+        useJUnitPlatform {
+            includeEngines("spek2")
+        }
+        testLogging.events("passed", "skipped", "failed")
+    }
+}
