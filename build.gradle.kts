a6914d806a7f52f7b16ddfe1bb293232800ea92b
Joakim kartveit <joakim.kartveit@nav.no>
2019-04-05 12:42:16 +0200

    Removed snapshot version

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ import no.nils.wsdl2java.Wsdl2JavaTask
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 group = "no.nav.syfo"
-version = "1.0.16-SNAPSHOT"
+version = "1.0.16"
 
 val coroutinesVersion = "1.0.0"
 val javaxActivationVersion = "1.1.1"
