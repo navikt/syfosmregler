4b13464f3ebf0e60a16c9a77cc4e69bfb232ebc0
Tia Firing <tia.firing@nav.no>
2019-08-19 14:21:25 +0200

    fjerner buildscript

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,18 +37,6 @@ plugins {
     id("com.github.johnrengelman.shadow") version "5.1.0"
 }
 
-
-buildscript {
-    dependencies {
-        classpath("javax.xml.bind:jaxb-api:2.4.0-b180830.0359")
-        classpath("org.glassfish.jaxb:jaxb-runtime:2.4.0-b180830.0438")
-        classpath("com.sun.activation:javax.activation:1.2.0")
-        classpath("com.sun.xml.ws:jaxws-tools:2.3.1") {
-            exclude(group = "com.sun.xml.ws", module = "policy")
-        }
-    }
-}
-
 repositories {
     maven(url = "https://dl.bintray.com/kotlin/ktor")
     maven(url = "https://dl.bintray.com/spekframework/spek-dev")
