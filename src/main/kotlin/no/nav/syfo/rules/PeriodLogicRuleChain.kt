940dd3c5c1644e5747b0ad9c9e5d8e24bcccc893
Joakim Kartveit <joakimkartveit@gmail.com>
2019-09-13 14:13:18 +0200

    Fjernet regelen SIGNATURDATO_ETTER_MOTTATT_DATO

--- src/main/kotlin/no/nav/syfo/rules/PeriodLogicRuleChain.kt
+++ src/main/kotlin/no/nav/syfo/rules/PeriodLogicRuleChain.kt
@@ -14,16 +14,6 @@ enum class PeriodLogicRuleChain(
     override val messageForSender: String,
     override val predicate: (RuleData<RuleMetadata>) -> Boolean
 ) : Rule<RuleData<RuleMetadata>> {
-    @Description("Behandlet dato (felt 12.1) er etter dato for mottak av sykmeldingen.")
-    SIGNATURDATO_ETTER_MOTTATT_DATO(
-            1123,
-            Status.INVALID,
-            "Den må ha riktig dato for behandlingen.",
-            "Behandlet dato (felt 12.1) er etter dato for mottak av sykmeldingen.",
-            { (healthInformation, ruleMetadata) ->
-        healthInformation.behandletTidspunkt > ruleMetadata.signatureDate.plusHours(3)
-    }),
-
     @Description("Hvis ingen perioder er oppgitt skal sykmeldingen avvises.")
     PERIODER_MANGLER(
             1200,
