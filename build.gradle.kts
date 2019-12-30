89cb48e783e8f6b221252be98f4e4f6e64c47871
Joakim Kartveit <joakimkartveit@gmail.com>
2019-12-30 09:57:23 +0100

    Endere måten vi deployer til nais plattformen. Oppgrader gradle Og legger til jonas i README.md

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import com.github.jengelman.gradle.plugins.shadow.transformers.ServiceFileTransf
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 group = "no.nav.syfo"
-version = "1.0.22"
+version = "1.0.0"
 
 val coroutinesVersion = "1.1.1"
 val javaxActivationVersion = "1.1.1"
@@ -152,5 +152,9 @@ tasks {
         testLogging {
             showStandardStreams = true
         }
+    }
+
+    "check" {
+        dependsOn("formatKotlin")
     }
 }
