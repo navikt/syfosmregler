2f71e405c28ef77378982b3182093d64692fb930
Joakim Kartveit <joakimkartveit@gmail.com>
2020-02-08 14:01:22 +0100

    Retter på teskten og regelen slik at det stemmer med reglen

--- src/main/kotlin/no/nav/syfo/rules/HPRRuleChain.kt
+++ src/main/kotlin/no/nav/syfo/rules/HPRRuleChain.kt
@@ -73,14 +73,14 @@ enum class HPRRuleChain(
         }
     }),
 
-    @Description("Hvis en sykmelding fra manuellterapeut/kiropraktor eller fysioterapeut overstiger 12 uker regnet fra første sykefraværsdag skal meldingen avvises")
+    @Description("Hvis en sykmelding fra manuellterapeut/kiropraktor eller fysioterapeut overstiger 12 uker regnet fra første fom dato til siste tom skal meldingen avvises")
     BEHANDLER_MT_FT_KI_OVER_12_UKER(
             1519,
             Status.INVALID,
             "Den som skrev sykmeldingen mangler autorisasjon.",
             "Sykmeldingen kan ikke rettes, det må skrives en ny. Pasienten har fått beskjed om å vente på ny sykmelding fra deg. Grunnet følgende:" +
-                    "Behandler er manuellterapeut/kiropraktor eller fysioterapeut overstiger 12 uker regnet fra første sykefraværsdag", { (healthInformation, behandler) ->
-        healthInformation.perioder.any { (it.fom..it.tom).daysBetween() > 84 } &&
+                    "Behandler er manuellterapeut/kiropraktor eller fysioterapeut overstiger 12 uker regnet fra første fom dato til siste tom dato", { (sykmelding, behandler) ->
+        (sykmelding.perioder.sortedFOMDate().first()..sykmelding.perioder.sortedTOMDate().last()).daysBetween() > 84 &&
             !harAktivHelsepersonellAutorisasjonsSom(behandler, listOf(
                 HelsepersonellKategori.LEGE.verdi,
                 HelsepersonellKategori.TANNLEGE.verdi)) &&
