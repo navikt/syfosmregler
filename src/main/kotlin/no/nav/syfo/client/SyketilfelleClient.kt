ebc5836cb3aaf59c413cbc78cb0cd68fe672f449
Tia Firing <tia.firing@nav.no>
2021-01-29 09:52:26 +0100

    henter startdato fra syfosyketilfelle

--- src/main/kotlin/no/nav/syfo/client/SyketilfelleClient.kt
+++ src/main/kotlin/no/nav/syfo/client/SyketilfelleClient.kt
@@ -2,6 +2,7 @@ package no.nav.syfo.client
 
 import io.ktor.client.HttpClient
 import io.ktor.client.request.accept
+import io.ktor.client.request.get
 import io.ktor.client.request.headers
 import io.ktor.client.request.post
 import io.ktor.http.ContentType
@@ -11,7 +12,10 @@ import java.time.LocalDate
 import net.logstash.logback.argument.StructuredArguments.fields
 import no.nav.syfo.LoggingMeta
 import no.nav.syfo.api.log
+import no.nav.syfo.model.Periode
 import no.nav.syfo.model.Syketilfelle
+import no.nav.syfo.rules.sortedFOMDate
+import no.nav.syfo.rules.sortedTOMDate
 
 @KtorExperimentalAPI
 class SyketilfelleClient(private val endpointUrl: String, private val stsClient: StsOidcClient, private val httpClient: HttpClient) {
@@ -33,8 +37,55 @@ class SyketilfelleClient(private val endpointUrl: String, private val stsClient:
             throw e
         }
     }
+
+    suspend fun finnStartdatoForSammenhengendeSyketilfelle(aktorId: String, periodeliste: List<Periode>, loggingMeta: LoggingMeta): LocalDate? {
+        log.info("Sjekker om nytt syketilfelle mot syfosyketilfelle {}", fields(loggingMeta))
+        val sykeforloep = hentSykeforloep(aktorId)
+
+        return finnStartdato(sykeforloep, periodeliste, loggingMeta)
+    }
+
+    fun finnStartdato(sykeforloep: List<Sykeforloep>, periodeliste: List<Periode>, loggingMeta: LoggingMeta): LocalDate? {
+        if (sykeforloep.isEmpty()) {
+            return null
+        }
+        val forsteFomIMottattSykmelding = periodeliste.sortedFOMDate().firstOrNull()
+        val sisteTomIMottattSykmelding = periodeliste.sortedTOMDate().lastOrNull()
+        if (forsteFomIMottattSykmelding == null || sisteTomIMottattSykmelding == null) {
+            log.warn("Mangler fom eller tom for sykmeldingsperioder: {}", fields(loggingMeta))
+            return null
+        }
+        val periodeRange = forsteFomIMottattSykmelding.rangeTo(sisteTomIMottattSykmelding)
+        val sammeSykeforloep = sykeforloep.firstOrNull {
+            it.sykmeldinger.any { simpleSykmelding -> simpleSykmelding.erSammeOppfolgingstilfelle(periodeRange) }
+        }
+        return sammeSykeforloep?.oppfolgingsdato
+    }
+
+    private fun SimpleSykmelding.erSammeOppfolgingstilfelle(periodeRange: ClosedRange<LocalDate>): Boolean {
+        if (fom.minusDays(16) in periodeRange || tom.plusDays(16) in periodeRange) {
+            return true
+        }
+        return false
+    }
+
+    private suspend fun hentSykeforloep(aktorId: String): List<Sykeforloep> =
+        httpClient.get<List<Sykeforloep>>("$endpointUrl/sparenaproxy/$aktorId/sykeforloep") {
+            accept(ContentType.Application.Json)
+            val oidcToken = stsClient.oidcToken()
+            headers {
+                append("Authorization", "Bearer ${oidcToken.access_token}")
+            }
+        }
 }
 
-data class Oppfolgingstilfelle(val antallBrukteDager: Int, val oppbruktArbeidsgvierperiode: Boolean, val arbeidsgiverperiode: Periode?)
+data class Sykeforloep(
+    var oppfolgingsdato: LocalDate,
+    val sykmeldinger: List<SimpleSykmelding>
+)
 
-data class Periode(val fom: LocalDate, val tom: LocalDate)
+data class SimpleSykmelding(
+    val id: String,
+    val fom: LocalDate,
+    val tom: LocalDate
+)
