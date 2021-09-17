8522da24b42365e74f451d2cd15c504a5524923a
Tia Firing <tia.firing@nav.no>
2021-09-17 13:23:25 +0200

    Revert "fjerner icd10-koder"
    This reverts commit 32f8c35cbf16264a05d6ab11580aecacba54dd72.

--- src/main/kotlin/no/nav/syfo/services/RuleService.kt
+++ src/main/kotlin/no/nav/syfo/services/RuleService.kt
@@ -142,7 +142,15 @@ fun gjelderBrudd(sykmelding: Sykmelding): Boolean {
         (sykmelding.medisinskVurdering.hovedDiagnose?.isICPC2() ?: false && sykmelding.medisinskVurdering.hovedDiagnose?.kode == "L73") ||
         (sykmelding.medisinskVurdering.hovedDiagnose?.isICPC2() ?: false && sykmelding.medisinskVurdering.hovedDiagnose?.kode == "L74") ||
         (sykmelding.medisinskVurdering.hovedDiagnose?.isICPC2() ?: false && sykmelding.medisinskVurdering.hovedDiagnose?.kode == "L75") ||
-        (sykmelding.medisinskVurdering.hovedDiagnose?.isICPC2() ?: false && sykmelding.medisinskVurdering.hovedDiagnose?.kode == "L76"))
+        (sykmelding.medisinskVurdering.hovedDiagnose?.isICPC2() ?: false && sykmelding.medisinskVurdering.hovedDiagnose?.kode == "L76") ||
+        (sykmelding.medisinskVurdering.hovedDiagnose?.isICpc10() ?: false && sykmelding.medisinskVurdering.hovedDiagnose?.kode?.startsWith("S12") ?: false) ||
+        (sykmelding.medisinskVurdering.hovedDiagnose?.isICpc10() ?: false && sykmelding.medisinskVurdering.hovedDiagnose?.kode?.startsWith("S22") ?: false) ||
+        (sykmelding.medisinskVurdering.hovedDiagnose?.isICpc10() ?: false && sykmelding.medisinskVurdering.hovedDiagnose?.kode?.startsWith("S32") ?: false) ||
+        (sykmelding.medisinskVurdering.hovedDiagnose?.isICpc10() ?: false && sykmelding.medisinskVurdering.hovedDiagnose?.kode?.startsWith("S42") ?: false) ||
+        (sykmelding.medisinskVurdering.hovedDiagnose?.isICpc10() ?: false && sykmelding.medisinskVurdering.hovedDiagnose?.kode?.startsWith("S52") ?: false) ||
+        (sykmelding.medisinskVurdering.hovedDiagnose?.isICpc10() ?: false && sykmelding.medisinskVurdering.hovedDiagnose?.kode?.startsWith("S62") ?: false) ||
+        (sykmelding.medisinskVurdering.hovedDiagnose?.isICpc10() ?: false && sykmelding.medisinskVurdering.hovedDiagnose?.kode?.startsWith("S72") ?: false) ||
+        (sykmelding.medisinskVurdering.hovedDiagnose?.isICpc10() ?: false && sykmelding.medisinskVurdering.hovedDiagnose?.kode?.startsWith("S82") ?: false))
 }
 
 fun kommerFraSykehus(sykmelding: Sykmelding): Boolean {
