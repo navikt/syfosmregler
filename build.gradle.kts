df6b41463976b8943e73ec3ecc9f243a5f4f9a63
Tia Firing <tia.firing@nav.no>
2021-09-20 10:38:45 +0200

    fjerner repoer

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,7 +31,6 @@ val githubPassword: String by project
 
 repositories {
     mavenCentral()
-    jcenter()
     maven {
         url = uri("https://maven.pkg.github.com/navikt/syfosm-common")
         credentials {
@@ -39,9 +38,6 @@ repositories {
             password = githubPassword
         }
     }
-    maven(url = "https://dl.bintray.com/kotlin/ktor")
-    maven(url = "https://dl.bintray.com/spekframework/spek-dev")
-    maven(url = "https://kotlin.bintray.com/kotlinx")
 }
 
 
