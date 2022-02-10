32e7f2308804a081b784df3c0505015b9eb86b1a
Tia Firing <tia.firing@nav.no>
2022-02-10 12:08:04 +0100

    krever autentisering

--- src/main/kotlin/no/nav/syfo/api/RuleApi.kt
+++ src/main/kotlin/no/nav/syfo/api/RuleApi.kt
@@ -3,8 +3,9 @@ package no.nav.syfo.api
 import io.ktor.application.call
 import io.ktor.request.receive
 import io.ktor.response.respond
-import io.ktor.routing.Routing
+import io.ktor.routing.Route
 import io.ktor.routing.post
+import kotlinx.coroutines.DelicateCoroutinesApi
 import no.nav.syfo.metrics.RULE_HIT_STATUS_COUNTER
 import no.nav.syfo.model.ReceivedSykmelding
 import no.nav.syfo.model.ValidationResult
@@ -14,7 +15,8 @@ import org.slf4j.LoggerFactory
 
 val log: Logger = LoggerFactory.getLogger("no.nav.syfo.smregler")
 
-fun Routing.registerRuleApi(ruleService: RuleService) {
+@DelicateCoroutinesApi
+fun Route.registerRuleApi(ruleService: RuleService) {
     post("/v1/rules/validate") {
         log.info("Got a request to validate rules")
 
--- src/main/kotlin/no/nav/syfo/application/ApplicationEngine.kt
+++ src/main/kotlin/no/nav/syfo/application/ApplicationEngine.kt
@@ -1,10 +1,12 @@
 package no.nav.syfo.application
 
+import com.auth0.jwk.JwkProvider
 import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import io.ktor.application.call
 import io.ktor.application.install
+import io.ktor.auth.authenticate
 import io.ktor.features.ContentNegotiation
 import io.ktor.features.StatusPages
 import io.ktor.http.HttpStatusCode
@@ -14,21 +16,31 @@ import io.ktor.routing.routing
 import io.ktor.server.engine.ApplicationEngine
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
+import kotlinx.coroutines.DelicateCoroutinesApi
 import no.nav.syfo.Environment
 import no.nav.syfo.api.registerRuleApi
 import no.nav.syfo.application.api.registerNaisApi
+import no.nav.syfo.application.authentication.setupAuth
 import no.nav.syfo.log
 import no.nav.syfo.services.RuleService
 
+@DelicateCoroutinesApi
 fun createApplicationEngine(
     ruleService: RuleService,
     env: Environment,
-    applicationState: ApplicationState
+    applicationState: ApplicationState,
+    jwkProviderAad: JwkProvider
 ): ApplicationEngine =
     embeddedServer(Netty, env.applicationPort) {
+        setupAuth(
+            environment = env,
+            jwkProviderAadV2 = jwkProviderAad
+        )
         routing {
             registerNaisApi(applicationState)
-            registerRuleApi(ruleService)
+            authenticate("servicebrukerAAD") {
+                registerRuleApi(ruleService)
+            }
         }
         install(ContentNegotiation) {
             jackson {
