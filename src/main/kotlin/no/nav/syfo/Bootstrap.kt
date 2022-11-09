a7259824cbc51800891863f96e0adc7d66801f58
Tia Firing <tia.firing@nav.no>
2022-11-09 16:02:14 +0100

    angir timeout og lavere ventetid for retry

--- src/main/kotlin/no/nav/syfo/Bootstrap.kt
+++ src/main/kotlin/no/nav/syfo/Bootstrap.kt
@@ -12,6 +12,7 @@ import io.ktor.client.engine.apache.Apache
 import io.ktor.client.engine.apache.ApacheEngineConfig
 import io.ktor.client.plugins.HttpRequestRetry
 import io.ktor.client.plugins.HttpResponseValidator
+import io.ktor.client.plugins.HttpTimeout
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.network.sockets.SocketTimeoutException
 import io.ktor.serialization.jackson.jackson
@@ -81,7 +82,7 @@ fun main() {
             }
         }
         install(HttpRequestRetry) {
-            constantDelay(100, 0, false)
+            constantDelay(50, 0, false)
             retryOnExceptionIf(3) { request, throwable ->
                 log.warn("Caught exception ${throwable.message}, for url ${request.url}")
                 true
@@ -94,6 +95,11 @@ fun main() {
                     false
                 }
             }
+        }
+        install(HttpTimeout) {
+            socketTimeoutMillis = 20_000
+            connectTimeoutMillis = 20_000
+            requestTimeoutMillis = 20_000
         }
     }
 
