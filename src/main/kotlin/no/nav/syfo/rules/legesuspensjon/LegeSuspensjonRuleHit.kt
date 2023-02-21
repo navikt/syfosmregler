cf3ea5ac79671bfade01d6902b490be6d58ae92f
Joakim Taule Kartveit <joakimkartveit@gmail.com>
2023-02-21 08:06:55 +0100

    [skip ci]

--- src/main/kotlin/no/nav/syfo/rules/legesuspensjon/LegeSuspensjonRuleHit.kt
+++ src/main/kotlin/no/nav/syfo/rules/legesuspensjon/LegeSuspensjonRuleHit.kt
@@ -8,7 +8,7 @@ enum class LegeSuspensjonRuleHit(
 ) {
     BEHANDLER_SUSPENDERT(
         ruleHit = RuleHit(
-            rule = BEHANDLER_SUSPENDERT.name,
+            rule = "BEHANDLER_SUSPENDERT",
             status = Status.INVALID,
             messageForSender = "Behandler er suspendert av NAV på konsultasjonstidspunkt. Pasienten har fått beskjed.",
             messageForUser = "Den som sykmeldte deg har mistet retten til å skrive sykmeldinger.",
