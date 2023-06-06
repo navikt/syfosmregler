3df36753eaae91449b322faa24cfcc75d191af6a
Joakim Taule Kartveit <joakimkartveit@gmail.com>
2023-06-06 11:59:07 +0200

    Added in metric INVALID_UTDYPENDE_OPPLYSINGER

--- src/main/kotlin/no/nav/syfo/services/RuleService.kt
+++ src/main/kotlin/no/nav/syfo/services/RuleService.kt
@@ -8,15 +8,19 @@ import no.nav.syfo.client.Behandler
 import no.nav.syfo.client.LegeSuspensjonClient
 import no.nav.syfo.client.NorskHelsenettClient
 import no.nav.syfo.client.SyketilfelleClient
+import no.nav.syfo.metrics.INVALID_UTDYPENDE_OPPLYSINGER
 import no.nav.syfo.metrics.RULE_NODE_RULE_HIT_COUNTER
 import no.nav.syfo.metrics.RULE_NODE_RULE_PATH_COUNTER
 import no.nav.syfo.model.Periode
 import no.nav.syfo.model.ReceivedSykmelding
 import no.nav.syfo.model.RuleInfo
 import no.nav.syfo.model.RuleMetadata
+import no.nav.syfo.model.SporsmalSvar
 import no.nav.syfo.model.Status
+import no.nav.syfo.model.Sykmelding
 import no.nav.syfo.model.ValidationResult
 import no.nav.syfo.pdl.service.PdlPersonService
+import no.nav.syfo.questions.QuestionGroup
 import no.nav.syfo.rules.common.RuleResult
 import no.nav.syfo.rules.dsl.TreeOutput
 import no.nav.syfo.rules.dsl.printRulePath
@@ -109,7 +113,11 @@ class RuleService(
         log.info("Avsender behandler har hprnummer: ${behandler.hprNummer}, {}", fields(loggingMeta))
 
         val ettersendingOgForlengelse = if (erTilbakedatert(receivedSykmelding)) {
-            sykmeldingService.getSykmeldingMetadataInfo(receivedSykmelding.personNrPasient, receivedSykmelding.sykmelding, loggingMeta)
+            sykmeldingService.getSykmeldingMetadataInfo(
+                receivedSykmelding.personNrPasient,
+                receivedSykmelding.sykmelding,
+                loggingMeta,
+            )
         } else {
             SykmeldingMetadataInfo(null, emptyList())
         }
@@ -129,6 +137,14 @@ class RuleService(
             ).inc()
         }
 
+        if (under3ordISvarPaaUtdypendeopplysingerSvarRegelsettVersjon3(
+                receivedSykmelding.sykmelding,
+                ruleMetadataSykmelding,
+            )
+        ) {
+            INVALID_UTDYPENDE_OPPLYSINGER.inc()
+        }
+
         juridiskVurderingService.processRuleResults(receivedSykmelding, result)
         val validationResult = validationResult(result.map { it.first })
         RULE_NODE_RULE_HIT_COUNTER.labels(
@@ -138,23 +154,24 @@ class RuleService(
         return validationResult
     }
 
-    private fun validationResult(results: List<TreeOutput<out Enum<*>, RuleResult>>): ValidationResult = ValidationResult(
-        status = results
-            .map { result -> result.treeResult.status }.let {
-                it.firstOrNull { status -> status == Status.INVALID }
-                    ?: it.firstOrNull { status -> status == Status.MANUAL_PROCESSING }
-                    ?: Status.OK
-            },
-        ruleHits = results.mapNotNull { it.treeResult.ruleHit }
-            .map { result ->
-                RuleInfo(
-                    result.rule,
-                    result.messageForSender,
-                    result.messageForUser,
-                    result.status,
-                )
-            },
-    )
+    private fun validationResult(results: List<TreeOutput<out Enum<*>, RuleResult>>): ValidationResult =
+        ValidationResult(
+            status = results
+                .map { result -> result.treeResult.status }.let {
+                    it.firstOrNull { status -> status == Status.INVALID }
+                        ?: it.firstOrNull { status -> status == Status.MANUAL_PROCESSING }
+                        ?: Status.OK
+                },
+            ruleHits = results.mapNotNull { it.treeResult.ruleHit }
+                .map { result ->
+                    RuleInfo(
+                        result.rule,
+                        result.messageForSender,
+                        result.messageForUser,
+                        result.status,
+                    )
+                },
+        )
 
     private fun erTilbakedatert(receivedSykmelding: ReceivedSykmelding): Boolean =
         receivedSykmelding.sykmelding.behandletTidspunkt.toLocalDate() > receivedSykmelding.sykmelding.perioder.sortedFOMDate()
@@ -180,3 +197,44 @@ fun List<Periode>.sortedTOMDate(): List<LocalDate> =
     map { it.tom }.sorted()
 
 fun ClosedRange<LocalDate>.daysBetween(): Long = ChronoUnit.DAYS.between(start, endInclusive)
+
+fun under3ordISvarPaaUtdypendeopplysingerSvarRegelsettVersjon3(
+    sykmelding: Sykmelding,
+    ruleMetadataSykmelding: RuleMetadataSykmelding,
+): Boolean {
+    val rulesetVersion = ruleMetadataSykmelding.ruleMetadata.rulesetVersion
+    val utdypendeOpplysninger = sykmelding.utdypendeOpplysninger
+
+    val group63 = utdypendeOpplysninger.containsMinWordsAnswersFor(QuestionGroup.GROUP_6_3, 3) == true &&
+        rulesetVersion in arrayOf("3")
+
+    val group64 = utdypendeOpplysninger.containsMinWordsAnswersFor(QuestionGroup.GROUP_6_4, 3) == true &&
+        rulesetVersion in arrayOf("3")
+
+    val group65 = utdypendeOpplysninger.containsMinWordsAnswersFor(QuestionGroup.GROUP_6_5, 3) == true &&
+        rulesetVersion in arrayOf("3")
+
+    return group63 || group64 || group65
+}
+
+fun Map<String, Map<String, SporsmalSvar>>.containsMinWordsAnswersFor(
+    questionGroup: QuestionGroup,
+    numberOfWords: Int,
+) =
+    this[questionGroup.spmGruppeId]?.all { (spmId, _) ->
+        spmId in questionGroup.spmsvar.map { it.spmId } &&
+            questionGroup.spmsvar.all { getNumberOfWords(it.spmTekst) > numberOfWords }
+    }
+
+fun Map<String, Map<String, SporsmalSvar>>.containsAnswersFor(questionGroup: QuestionGroup) =
+    this[questionGroup.spmGruppeId]?.all { (spmId, _) ->
+        spmId in questionGroup.spmsvar.map { it.spmId }
+    }
+
+fun getNumberOfWords(input: String?): Int {
+    return input?.trim()?.split(" ")?.filter { containsLetters(it) }?.size ?: 0
+}
+
+fun containsLetters(text: String): Boolean {
+    return text.contains("""[A-Za-z]""".toRegex())
+}
