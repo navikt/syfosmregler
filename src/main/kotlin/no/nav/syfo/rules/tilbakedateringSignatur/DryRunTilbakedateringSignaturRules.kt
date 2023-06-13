08438acb7f1941e363cfba995e64c58279162f19
Andreas <danduras@gmail.com>
2023-06-13 13:28:11 +0200

    Test hvor mange flere tilbakedateringer man vil få med signaturdato (#162)

---
+++ src/main/kotlin/no/nav/syfo/rules/tilbakedateringSignatur/DryRunTilbakedateringSignaturRules.kt
@@ -0,0 +1,74 @@
+package no.nav.syfo.rules.tilbakedateringSignatur
+
+import no.nav.syfo.metrics.TILBAKEDATERING_SIGNATUR_RULE_HIT_COUNTER
+import no.nav.syfo.metrics.TILBAKEDATERING_SIGNATUR_RULE_PATH_COUNTER
+import no.nav.syfo.model.RuleInfo
+import no.nav.syfo.model.Status
+import no.nav.syfo.model.Sykmelding
+import no.nav.syfo.model.ValidationResult
+import no.nav.syfo.rules.common.Juridisk
+import no.nav.syfo.rules.common.RuleExecution
+import no.nav.syfo.rules.common.RuleResult
+import no.nav.syfo.rules.dsl.TreeOutput
+import no.nav.syfo.rules.dsl.printRulePath
+import no.nav.syfo.services.RuleMetadataSykmelding
+
+private val ruleExecution = sequenceOf(
+    TilbakedateringSignaturRulesExecution(tilbakedateringSignaturRuleTree),
+)
+
+private fun runRules(
+    sykmelding: Sykmelding,
+    ruleMetadataSykmelding: RuleMetadataSykmelding,
+    sequence: Sequence<RuleExecution<out Enum<*>>> = ruleExecution,
+): List<Pair<TreeOutput<out Enum<*>, RuleResult>, Juridisk>> {
+    var lastStatus = Status.OK
+    val results = sequence
+        .map { it.runRules(sykmelding, ruleMetadataSykmelding) }
+        .takeWhile {
+            if (lastStatus == Status.OK) {
+                lastStatus = it.first.treeResult.status
+                true
+            } else {
+                false
+            }
+        }
+    return results.toList()
+}
+
+fun dryRunTilbakedateringSignatur(
+    sykmelding: Sykmelding,
+    ruleMetadataSykmelding: RuleMetadataSykmelding,
+) {
+    val dryRuns = runRules(sykmelding, ruleMetadataSykmelding)
+    dryRuns.forEach {
+        TILBAKEDATERING_SIGNATUR_RULE_PATH_COUNTER.labels(
+            it.first.printRulePath(),
+        ).inc()
+    }
+
+    val validationResult = validationResult(dryRuns.map { it.first })
+    TILBAKEDATERING_SIGNATUR_RULE_HIT_COUNTER.labels(
+        validationResult.status.name,
+        validationResult.ruleHits.firstOrNull()?.ruleName ?: validationResult.status.name,
+    ).inc()
+}
+
+private fun validationResult(results: List<TreeOutput<out Enum<*>, RuleResult>>): ValidationResult =
+    ValidationResult(
+        status = results
+            .map { result -> result.treeResult.status }.let {
+                it.firstOrNull { status -> status == Status.INVALID }
+                    ?: it.firstOrNull { status -> status == Status.MANUAL_PROCESSING }
+                    ?: Status.OK
+            },
+        ruleHits = results.mapNotNull { it.treeResult.ruleHit }
+            .map { result ->
+                RuleInfo(
+                    result.rule,
+                    result.messageForSender,
+                    result.messageForUser,
+                    result.status,
+                )
+            },
+    )
---
+++ src/main/kotlin/no/nav/syfo/rules/tilbakedateringSignatur/TilbakedateringSignaturRuleHit.kt
@@ -0,0 +1,68 @@
+package no.nav.syfo.rules.tilbakedateringSignatur
+
+import no.nav.syfo.model.Status
+import no.nav.syfo.rules.common.RuleHit
+
+enum class TilbakedateringSignaturRuleHit(
+    val ruleHit: RuleHit,
+) {
+    INNTIL_8_DAGER(
+        ruleHit = RuleHit(
+            rule = "INNTIL_8_DAGER",
+            status = Status.INVALID,
+            messageForSender = "Sykmeldingen kan ikke rettes, det må skrives en ny. Pasienten har fått beskjed om å vente på ny sykmelding fra deg. Grunnet følgende:" +
+                "Første sykmelding er tilbakedatert uten at begrunnelse (felt 11.2) er tilstrekkelig utfylt",
+            messageForUser = "Sykmeldingen er tilbakedatert uten tilstrekkelig begrunnelse fra den som sykmeldte deg.",
+        ),
+    ),
+    INNTIL_30_DAGER(
+        ruleHit = RuleHit(
+            rule = "INNTIL_30_DAGER",
+            status = Status.INVALID,
+            messageForSender = "Sykmeldingen kan ikke rettes, det må skrives en ny. Pasienten har fått beskjed om å vente på ny sykmelding fra deg. Grunnet følgende:" +
+                "Sykmelding er tilbakedatert uten begrunnelse (felt 11.2) er tilstrekkelig utfylt",
+            messageForUser = "Sykmeldingen er tilbakedatert uten tilstrekkelig begrunnelse fra den som sykmeldte deg.",
+        ),
+    ),
+    INNTIL_30_DAGER_MED_BEGRUNNELSE(
+        ruleHit = RuleHit(
+            rule = "INNTIL_30_DAGER_MED_BEGRUNNELSE",
+            status = Status.MANUAL_PROCESSING,
+            messageForSender = "Første sykmelding er tilbakedatert og felt 11.2 (begrunnelse) er utfylt",
+            messageForUser = "Sykmeldingen blir manuelt behandlet fordi den er tilbakedatert",
+        ),
+    ),
+    OVER_30_DAGER(
+        ruleHit = RuleHit(
+            rule = "OVER_30_DAGER",
+            status = Status.INVALID,
+            messageForSender = "Sykmeldingen kan ikke rettes, det må skrives en ny. Pasienten har fått beskjed om å vente på ny sykmelding fra deg. " +
+                "Grunnet følgende: Sykmelding er tilbakedatert mer enn det som er tillatt og felt 11.2 (begrunnelse) er utfylt uten tilstrekkelig begrunnelse",
+            messageForUser = "Sykmeldingen er tilbakedatert uten tilstrekkelig begrunnelse fra den som sykmeldte deg.",
+        ),
+    ),
+    OVER_30_DAGER_MED_BEGRUNNELSE(
+        ruleHit = RuleHit(
+            rule = "OVER_30_DAGER_MED_BEGRUNNELSE",
+            status = Status.MANUAL_PROCESSING,
+            messageForSender = "Sykmeldingen er tilbakedatert og felt 11.2 (begrunnelse) er utfylt",
+            messageForUser = "Sykmeldingen blir manuell behandlet fordi den er tilbakedatert",
+        ),
+    ),
+    OVER_30_DAGER_SPESIALISTHELSETJENESTEN(
+        ruleHit = RuleHit(
+            rule = "OVER_30_DAGER_SPESIALISTHELSETJENESTEN",
+            status = Status.MANUAL_PROCESSING,
+            messageForSender = "Sykmeldingen er tilbakedatert over 30 dager og er fra spesialisthelsetjenesten",
+            messageForUser = "Sykmeldingen blir manuelt behandlet fordi den er tilbakedatert",
+        ),
+    ),
+    HOVEDDIAGNOSE_MANGLER(
+        ruleHit = RuleHit(
+            rule = "HOVEDDIAGNOSE_MANGLER",
+            status = Status.INVALID,
+            messageForSender = "Sykmeldingen er tilbakedatert og det er ikkje oppgitt houveddiagnose",
+            messageForUser = "Sykmeldingen er tilbakedatert, og det mangler diagnose fra den som sykmeldte deg.",
+        ),
+    ),
+}
