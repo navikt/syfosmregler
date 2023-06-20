afd2524b9afe08514ca714d45ae3093ecfc1d601
Joakim Taule Kartveit <joakim.taule.kartveit@nav.no>
2023-06-20 13:00:08 +0200

    added in org.cyclonedx.bom plugin for salsa

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,6 +26,7 @@ plugins {
     kotlin("jvm") version "1.8.22"
     id("com.diffplug.spotless") version "6.19.0"
     id("com.github.johnrengelman.shadow") version "8.1.1"
+    id("org.cyclonedx.bom") version "1.7.4
 }
 
 val githubUser: String by project
