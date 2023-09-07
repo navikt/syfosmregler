f9a7d39f140c2432301cb8617b85b804d4d051a4
Joakim Taule Kartveit <joakim.taule.kartveit@nav.no>
2023-09-07 07:47:33 +0200

    Removed plugin org.cyclonedx.bom noe longer nedded

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,7 +27,6 @@ plugins {
     kotlin("jvm") version "1.9.0"
     id("com.diffplug.spotless") version "6.20.0"
     id("com.github.johnrengelman.shadow") version "8.1.1"
-    id("org.cyclonedx.bom") version "1.7.4"
 }
 
 application {
