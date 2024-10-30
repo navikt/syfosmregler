433db54bfb54a30a0178d6e1511929bd3f362d82
Joakim Taule Kartveit <joakim.taule.kartveit@nav.no>
2024-10-30 08:44:23 +0100

    chor: use com.gradleup.shadow for shadowJar (#277)

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,7 +26,7 @@ plugins {
     id("application")
     kotlin("jvm") version "2.0.20"
     id("com.diffplug.spotless") version "6.25.0"
-    id("com.github.johnrengelman.shadow") version "8.1.1"
+    id("com.gradleup.shadow") version "8.3.3"
 }
 
 application {
