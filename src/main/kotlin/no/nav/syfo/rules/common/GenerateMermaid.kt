9e985e410fdc8e1f3a0f98572c426965a53ae718
Andreas Sagen Aspaas <danduras@gmail.com>
2025-04-01 07:32:52 +0200

    chore: update juridisk tekst

--- src/main/kotlin/no/nav/syfo/rules/common/GenerateMermaid.kt
+++ src/main/kotlin/no/nav/syfo/rules/common/GenerateMermaid.kt
@@ -32,10 +32,36 @@ fun main() {
 
         // separator
         builder.append("---\n\n")
+        val treeStringBuilder = StringBuilder()
+        val juridiskHenvisninger = mutableListOf<Juridisk>()
+        ruleTree.traverseTree(
+            treeStringBuilder,
+            thisNodeKey = "root",
+            nodeKey = "root",
+            juridiskHenvisninger
+        )
 
+        val henvisninger =
+            juridiskHenvisninger.filterIsInstance<MedJuridisk>().distinctBy {
+                it.juridiskHenvisning.paragraf
+            }
+
+        if (henvisninger.size > 1) {
+            throw RuntimeException(
+                "Juridisk henvisning has more than one paragraphs: ${henvisninger.joinToString { it.juridiskHenvisning.paragraf }}"
+            )
+        }
+
+        val henvisning = henvisninger.singleOrNull()
+
+        if (henvisning != null) {
+            builder.append("- ### Juridisk Henvisning:\n")
+            henvisning.juridiskHenvisning.lovverk.let { builder.append("  - **Lovverk**: $it\n") }
+            henvisning.juridiskHenvisning.paragraf.let { builder.append("  - **Paragraf**: $it\n") }
+        }
         builder.append("```mermaid\n")
         builder.append("graph TD\n")
-        ruleTree.traverseTree(builder, "root", "root")
+        builder.append(treeStringBuilder)
         builder.append("    classDef ok fill:#c3ff91,stroke:#004a00,color: black;\n")
         builder.append("    classDef invalid fill:#ff7373,stroke:#ff0000,color: black;\n")
         builder.append("    classDef manuell fill:#ffe24f,stroke:#ffd500,color: #473c00;\n")
@@ -49,6 +75,7 @@ private fun <T> TreeNode<T, RuleResult>.traverseTree(
     builder: StringBuilder,
     thisNodeKey: String,
     nodeKey: String,
+    juridiskHenvisninger: MutableList<Juridisk>,
 ) {
     when (this) {
         is ResultNode -> {
@@ -58,28 +85,39 @@ private fun <T> TreeNode<T, RuleResult>.traverseTree(
         is RuleNode -> {
             val currentNodeKey = "${nodeKey}_$rule"
             if (yes is ResultNode) {
-                val childResult = (yes as ResultNode<T, RuleResult>).result.status
-                val childKey = "${currentNodeKey}_$childResult"
+                val childResult = (yes as ResultNode<T, RuleResult>).result
+                val childKey = "${currentNodeKey}_${childResult.status}"
+                juridiskHenvisninger.add(childResult.juridisk)
+                val result =
+                    "${childResult.status}\n${genererLovhenvisning(childResult.juridisk)}"
                 builder.append(
-                    "    $thisNodeKey($rule) -->|Yes| $childKey($childResult)${getStyle(childResult)}\n"
+                    "    $thisNodeKey($rule) -->|Yes| $childKey($result)${getStyle(childResult.status)}\n"
                 )
             } else {
                 val childRule = (yes as RuleNode<T, RuleResult>).rule
                 val childKey = "${currentNodeKey}_$childRule"
                 builder.append("    $thisNodeKey($rule) -->|Yes| $childKey($childRule)\n")
-                yes.traverseTree(builder, childKey, currentNodeKey)
+                yes.traverseTree(builder, childKey, currentNodeKey, juridiskHenvisninger)
             }
             if (no is ResultNode) {
-                val childResult = (no as ResultNode<T, RuleResult>).result.status
-                val childKey = "${currentNodeKey}_$childResult"
+                val childResult = (no as ResultNode<T, RuleResult>).result
+                juridiskHenvisninger.add(childResult.juridisk)
+                val childKey = "${currentNodeKey}_${childResult.status}"
+                val result =
+                    "${childResult.status} <br/>" + genererLovhenvisning(childResult.juridisk)
                 builder.append(
-                    "    $thisNodeKey($rule) -->|No| $childKey($childResult)${getStyle(childResult)}\n"
+                    "    $thisNodeKey($rule) -->|No| $childKey(${result})${getStyle(childResult.status)}\n"
                 )
             } else {
                 val childRule = (no as RuleNode<T, RuleResult>).rule
                 val childKey = "${currentNodeKey}_$childRule"
                 builder.append("    $thisNodeKey($rule) -->|No| $childKey($childRule)\n")
-                no.traverseTree(builder, "${currentNodeKey}_$childRule", currentNodeKey)
+                no.traverseTree(
+                    builder,
+                    "${currentNodeKey}_$childRule",
+                    currentNodeKey,
+                    juridiskHenvisninger
+                )
             }
         }
     }
@@ -91,3 +129,18 @@ fun getStyle(childResult: Status): String =
         Status.INVALID -> ":::invalid"
         Status.MANUAL_PROCESSING -> ":::manuell"
     }
+
+fun genererLovhenvisning(juridisk: Juridisk): String {
+    return when (juridisk) {
+        is UtenJuridisk -> ""
+        is MedJuridisk -> {
+            val henvisning = juridisk.juridiskHenvisning
+
+            val leddDel = henvisning.ledd?.let { "$it. ledd" }
+            val punktumDel = henvisning.punktum?.let { "$it. punktum" }
+            val bokstavDel = henvisning.bokstav?.let { "bokstav $it" }
+
+            return listOfNotNull(leddDel, punktumDel, bokstavDel).joinToString(" ")
+        }
+    }
+}
